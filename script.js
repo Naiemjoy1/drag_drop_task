56780c9723e4d72641efff0c854725982af871a3
naiemjoy1 <naiemunhasan@gmail.com>
2024-10-16 12:55:25 +0600

    task drag and drop any task box

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+let taskIdCounter = 7;
+
 function addCard(boxId) {
   const task = prompt("Enter the new task");
   if (task) {
     const boxContent = document.getElementById(boxId);
     const newTask = document.createElement("p");
     newTask.textContent = task;
+    newTask.setAttribute("id", "task" + taskIdCounter);
+    newTask.setAttribute("draggable", "true");
+    newTask.setAttribute("ondragstart", "drag(event)");
     newTask.setAttribute("onclick", "editTask(this)");
     boxContent.appendChild(newTask);
+    taskIdCounter++;
   }
 }
 
@@ -23,4 +29,24 @@ function editTask(element) {
       element.contentEditable = false;
     }
   });
+}
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drag(event) {
+  event.dataTransfer.setData("text/plain", event.target.id);
+}
+
+function drop(event) {
+  event.preventDefault();
+  const taskId = event.dataTransfer.getData("text/plain");
+  const task = document.getElementById(taskId);
+
+  if (event.target.classList.contains("box-content")) {
+    event.target.appendChild(task);
+  } else if (event.target.parentElement.classList.contains("box-content")) {
+    event.target.parentElement.appendChild(task);
+  }
 }
