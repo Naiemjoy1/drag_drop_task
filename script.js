5db767a4188c8982d420e1e45f749167ff83a2a4
naiemjoy1 <naiemunhasan@gmail.com>
2024-10-16 11:59:02 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function addCard(boxId) {
+  const task = prompt("Enter the new task");
+  if (task) {
+    const boxContent = document.getElementById(boxId);
+    const newTask = document.createElement("p");
+    newTask.textContent = task;
+    boxContent.appendChild(newTask);
+  }
+}
+
+function editTask(element) {
+  element.contentEditable = true;
+  element.focus();
+
+  element.addEventListener("blur", function () {
+    element.contentEditable = false;
+  });
+
+  element.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      element.contentEditable = false;
+    }
+  });
+}
